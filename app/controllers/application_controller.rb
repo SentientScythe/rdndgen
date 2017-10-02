c5f3b5673632e2cf4c0803df5bbb9bd7f7cf3bb4
Scott Lamb <sentientscythe@gmail.com>
2017-10-01 22:06:56 -0600

    Changed hot dog to Jessica Simpson

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-	render html: "Sup, my hot dogs"
+	render html: "Does anyone have a late-2000s picture of Jessica Simpson? Please! I'm desperate!"
   end
 end
