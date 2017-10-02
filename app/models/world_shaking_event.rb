28c562724d3dc79add08b2ec933a6e5f48f36a16
Scott Lamb <sentientscythe@gmail.com>
2017-10-02 00:13:26 -0600

    Generated a model for world shaking events and performed first migration. Cleaned up rspec gems.

---
+++ app/models/world_shaking_event.rb
@@ -0,0 +1,2 @@
+class WorldShakingEvent < ApplicationRecord
+end
---
+++ db/migrate/20171002052256_create_world_shaking_events.rb
@@ -0,0 +1,11 @@
+class CreateWorldShakingEvents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :world_shaking_events do |t|
+      t.string :type
+      t.string :subtype
+      t.string :text
+
+      t.timestamps
+    end
+  end
+end
